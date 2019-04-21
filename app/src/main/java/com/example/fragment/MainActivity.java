5562d4e6c6a85fdef21e63887599430b0cb3481f
Softwarica College Lab <softwarica@softwarica.com>
2019-04-21 11:33:00 +0545

    Add and Radius

--- app/src/main/java/com/example/fragment/MainActivity.java
+++ app/src/main/java/com/example/fragment/MainActivity.java
@@ -1,13 +1,45 @@
 package com.example.fragment;
 
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+import Fragment.FirstFragment;
+import Fragment.SecondFragment;
 
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    private Button btnFrag;
+    private Boolean status =true;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnFrag=findViewById(R.id.btnfrag);
+        btnFrag.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        FragmentManager fragmentManager =getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction=fragmentManager.beginTransaction();
+
+        if (status) {
+            FirstFragment firstFragment = new FirstFragment();
+            fragmentTransaction.replace(R.id.fragmentContainer, firstFragment);
+            fragmentTransaction.commit();
+            btnFrag.setText("Load Second Fragment");
+            status = false;
+        }else {
+            SecondFragment secondFragment =new SecondFragment();
+            fragmentTransaction.replace(R.id.fragmentContainer, secondFragment);
+            fragmentTransaction.commit();
+            btnFrag.setText("Load First Fragment");
+            status = true;
+        }
     }
 }
